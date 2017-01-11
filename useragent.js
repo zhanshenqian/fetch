3dfde9289c7b75c0556d3180b3b5193bccb9d6f9
zhanshenqian <1047315142@qq.com>
2017-01-11 17:56:04 +0800

    1
    delete

--- useragent.js
+++ useragent.js
@@ -2,12 +2,12 @@
 /*
 是否是浪客直播APP
 */
-export const IS_LIVE = navigator.userAgent.toLowerCase().indexOf('eastmoney_live') !== -1 || navigator.userAgent.toLowerCase().indexOf('eastmoney_emlive') !== -1
+export const IS_LIVE = navigator.userAgent.toLowerCase().indexOf('') !== -1 || navigator.userAgent.toLowerCase().indexOf('') !== -1
 
 /*
 是否是东财APP
 */
-export const IS_EASTMONEY = navigator.userAgent.toLowerCase().indexOf('eastmoney') !== -1
+export const IS_EASTMONEY = navigator.userAgent.toLowerCase().indexOf('') !== -1
 
 /*
 是否是微信
