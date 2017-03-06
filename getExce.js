88ddf86d43c9370e5037eed5388b4725762c49f1
zhanshenqian <1047315142@qq.com>
2017-03-06 09:52:47 +0800

    jsonToCvs
    jsonToCvs

--- getExce.js
+++ getExce.js
@@ -1,51 +1,33 @@
-      JSONToCSVConvertor(this.tableData, 'sd')
-      function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) { 
-        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-        var CSV = ''; 
-        CSV = '时间,名称,地址\r\n'   
-        //This condition will generate the Label/Header
-        if (ShowLabel) {
-            var row = "";
-            //This loop will extract the label from 1st index of on array
-            for (var index in arrData[0]) {
-                //Now convert each value to string and comma-seprated
-                row += index + ',';
-            }
-            // append Label row with line break
-            CSV += row.slice(0, -1) + '\r\n';
-        }
-        //1st loop is to extract each row
-        for (var i = 0; i < arrData.length; i++) {
-          var row = "";
-          //2nd loop will extract each column and convert it in string comma-seprated
-          for (var index in arrData[i]) {
-              row += '"' + arrData[i][index] + '",';
-          }
-          row.slice(0, row.length - 1);
-          //add a line break after each row
-          CSV += row + '\r\n';
-        }
-        if (CSV == '') {        
-            alert("Invalid data");
-            return;
-        }   
-        //this trick will generate a temp "a" tag
-        var link = document.createElement("a");    
-         link.id="lnkDwnldLnk";
-         
-        //this part will append the anchor tag and remove it after automatic click
-        document.body.appendChild(link);   
-        var csv = CSV;  
-        var blob = new Blob([csv], { type: 'text/csv' }); 
-        var csvUrl = window.webkitURL.createObjectURL(blob);
-        var filename = 'UserExport.csv';
-        $("#lnkDwnldLnk")
-        .attr({
-            'download': filename,
-            'href': csvUrl
-        }); 
-     
-        $('#lnkDwnldLnk')[0].click();    
-        document.body.removeChild(link);
-      }
+/**
+ * Set Date to string
+ * @param {JSONData} json
+ * @param {ReportTitle} title
+ * @param {TableHeader} header
+ */
+/* eslint-disable */
+export const JsonToCSV = (JSONData, ReportTitle, TableHeader) => {
+  let arrData = typeof JSONData !== 'object' ? JSON.parse(JSONData) : JSONData
+  let CSV = TableHeader ? TableHeader.replace(/[，]/g, ',') + '\r\n' : ''
+  for (let i = 0; i < arrData.length; i++) {
+    let row = ''
+    for (let index in arrData[i]) {
+      row += '"' + arrData[i][index] + '",'
+    }
+    row.slice(0, row.length - 1)
+    CSV += row + '\r\n'
+  }
+  if (CSV === '') {
+    alert('Invalid data')
+    return
+  }
+  let filename = ReportTitle ? ReportTitle.replace(/[ ]/g, '_') : 'Report'
+  let link = document.createElement('a')
+  document.body.appendChild(link)
+  let csv = CSV
+  let blob = new Blob([csv], { type: 'text/csv' })
+  let csvUrl = window.webkitURL.createObjectURL(blob)
+  link.download = filename + '.csv'
+  link.href = csvUrl
+  link.click()
+  document.body.removeChild(link)
+}
